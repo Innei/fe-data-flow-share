c103010285083395a15ef6a41564e99038647b7c
Innei <i@innei.in>
2024-07-20 22:42:32 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- src/slides/2.0.ExternalState.tsx
+++ src/slides/2.0.ExternalState.tsx
@@ -14,17 +14,48 @@ export default function Page() {
           <p className="text-xl">
             <code>`useSyncExternalStore`</code>
           </p>
-          <ShikiHighLighter
-            code={`import { useSyncExternalStore } from 'react';
+          <div className="grid gap-3 grid-cols-2">
+            <ShikiHighLighter
+              code={`import { useSyncExternalStore } from 'react';
 import { todosStore } from './todoStore.js';
 
 function TodosApp() {
   const todos = useSyncExternalStore(todosStore.subscribe, todosStore.getSnapshot);
   // ...
 }`}
-            language="tsx"
-          />
+              language="tsx"
+            />
+            <ShikiHighLighter
+              code={`let nextId = 0;
+let todos = [{ id: nextId++, text: 'Todo #1' }];
+let listeners = [];
 
+export const todosStore = {
+  addTodo() {
+    todos = [...todos, { id: nextId++, text: 'Todo #' + nextId }]
+    emitChange();
+  },
+  subscribe(listener) {
+    listeners = [...listeners, listener];
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  },
+  getSnapshot() {
+    return todos;
+  }
+};
+
+function emitChange() {
+  for (let listener of listeners) {
+    listener();
+  }
+}
+`.trim()}
+              className="max-h-[300px] overflow-auto"
+              language="ts"
+            ></ShikiHighLighter>
+          </div>
           <p className="text-xl">通过订阅数据源，更新 UI。</p>
         </div>
       }
